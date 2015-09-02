00fdbdd1d0c60c2fe19f8287ac29576c00b4d7c7
Ruti Wajnberg <rutiwajnberg@Rutis-MacBook-Air.local>
2015-09-02 14:10:44 -0400

    Move to triple nested hash and remove if/else logic for which hash to access.

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,41 +1,23 @@
 # Normal mode
 
-#NEEDS RANGES
+#TO DO:
+# Ace isn't working
+# Ranges within hashes aren't working --> speed up the creation, not the calling
+# If/Else logic for which hash to go to isn't working
+# Need hashes for hard and pair
+# nice to have: return error outside of worth range (2-10, A or 11)
 
-#soft_hash = {"dealer" => {"total" => "action"}}
-#soft_hash[dealer][total] = action
-#soft_hash[2][13] = "hit"
+#n_hash[dealer][total] = action
+blackjack_hash = Hash.new
+blackjack_hash = {"soft" => {2 => {13 => "hit"}}}
 
-soft_hash = {2 => {13 => "hit", 17 => "double hit", 18 => "stand"},
-            4 => {13 => "double hit", 19 => "stand"},
-            7 => {13 => "hit", 18 => "stand"}}
-
-puts "This is a game of Blackjack. Please enter the worth of the card - between 2 and 10 or A, which is 11."
-puts "IMPORTANT: For face cards (Jack, Queen, King), please enter 10."
-
+puts "This is a game of Blackjack. Please enter the worth of the card - 2 to 10 or A, which is 11."
 puts "Please enter your first card."
 first = gets.chomp.to_i
-
 puts "Please enter your second card."
 second = gets.chomp.to_i
-
 puts "Please enter the dealer's card."
 dealer = gets.chomp.to_i
 
-#puts "Your optimal move is #{action}!"
-
 total = first + second
-#soft_hash[dealer][total] = action
-
-puts soft_hash[dealer][total]
-
-#soft_hash[dealer] = total
-#puts soft_hash
-
-#if first == ace || second == ace
-#  use soft_hash
-#elsif first == second
-#  use pairs_hash
-#else
-#  use hard_hash
-#end
+puts "Your optimal move is #{blackjack_hash["soft"][dealer][total]}!"
