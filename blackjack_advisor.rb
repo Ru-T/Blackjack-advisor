1d7e73f40840d9efaaa0aa34e43344d6d9c3f8d8
Ruti Wajnberg <rutiwajnberg@Rutis-MacBook-Air.local>
2015-09-02 13:48:59 -0400

    Added a hash for soft hands and got action to respond.

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,16 +1,14 @@
 # Normal mode
 
-# add if user enters a number outside of the range, return an error.
-# fix that ace = 11 - not working below
-
-
-#hard_hash = Hash.new
-
-#pairs_hash = Hash.new
+#NEEDS RANGES
 
 #soft_hash = {"dealer" => {"total" => "action"}}
 #soft_hash[dealer][total] = action
 #soft_hash[2][13] = "hit"
+
+soft_hash = {2 => {13 => "hit", 17 => "double hit", 18 => "stand"},
+            4 => {13 => "double hit", 19 => "stand"},
+            7 => {13 => "hit", 18 => "stand"}}
 
 puts "This is a game of Blackjack. Please enter the worth of the card - between 2 and 10 or A, which is 11."
 puts "IMPORTANT: For face cards (Jack, Queen, King), please enter 10."
@@ -27,12 +25,12 @@ dealer = gets.chomp.to_i
 #puts "Your optimal move is #{action}!"
 
 total = first + second
+#soft_hash[dealer][total] = action
 
-soft_hash = Hash.new
-soft_hash[dealer] = Hash.new
-soft_hash[dealer][total] = first # for now, should be action
+puts soft_hash[dealer][total]
+
 #soft_hash[dealer] = total
-puts soft_hash
+#puts soft_hash
 
 #if first == ace || second == ace
 #  use soft_hash
