4dd41f8a7867bafd06ff18b3d6522db712496d92
Ruti Wajnberg <rutiwajnberg@Rutis-MacBook-Air.local>
2015-09-02 16:14:37 -0400

    Add soft hand hash.

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,9 +1,6 @@
 # Normal mode
 
-#TO DO:
-# If/Else to determine which hash within the hash (soft, hard, pairs)
 # Need to create all three hashes >> Ranges within hashes aren't working --> speed up the creation, not the calling
-
 # nice to have: return error outside of worth range (2-10, A or 11)
 
 def evaluate_card (card)
@@ -15,7 +12,18 @@ def evaluate_card (card)
 end
 
 blackjack_hash = Hash.new
-blackjack_hash = {"soft" => {2 => {13 => "HIT"}}, "hard" => {3 => {15 => "STAND"}}, "pairs" => {5 => {14 => "DOUBLE HIT"}}}
+#blackjack_hash = {"soft" => {2 => {13 => "HIT"}}, "hard" => {3 => {15 => "STAND"}}, "pairs" => {5 => {14 => "DOUBLE HIT"}}}
+blackjack_hash = {"soft" =>
+  {2 => {13 => "HIT", 14 => "HIT", 15 => "HIT", 16 => "HIT", 17 => "DOUBLE HIT", 18 => "STAND", 19 => "STAND", 20 => "STAND", 21 => "STAND"},
+   3 => {13 => "HIT", 14 => "HIT", 15 => "HIT", 16 => "HIT", 17 => "DOUBLE HIT", 18 => "DOUBLE STAND", 19 => "STAND", 20 => "STAND", 21 => "STAND"},
+   4 => {13 => "DOUBLE HIT", 14 => "DOUBLE HIT", 15 => "DOUBLE HIT", 16 => "DOUBLE HIT", 17 => "DOUBLE HIT", 18 => "DOUBLE STAND", 19 => "STAND", 20 => "STAND", 21 => "STAND"},
+   5 => {13 => "DOUBLE HIT", 14 => "DOUBLE HIT", 15 => "DOUBLE HIT", 16 => "DOUBLE HIT", 17 => "DOUBLE HIT", 18 => "DOUBLE STAND", 19 => "STAND", 20 => "STAND", 21 => "STAND"},
+   6 => {13 => "DOUBLE HIT", 14 => "DOUBLE HIT", 15 => "DOUBLE HIT", 16 => "DOUBLE HIT", 17 => "DOUBLE HIT", 18 => "DOUBLE STAND", 19 => "DOUBLE STAND", 20 => "STAND", 21 => "STAND"},
+   7 => {13 => "HIT", 14 => "HIT", 15 => "HIT", 16 => "HIT", 17 => "HIT", 18 => "STAND", 19 => "STAND", 20 => "STAND", 21 => "STAND"},
+   8 => {13 => "HIT", 14 => "HIT", 15 => "HIT", 16 => "HIT", 17 => "HIT", 18 => "STAND", 19 => "STAND", 20 => "STAND", 21 => "STAND"},
+   9 => {13 => "HIT", 14 => "HIT", 15 => "HIT", 16 => "HIT", 17 => "HIT", 18 => "HIT", 19 => "STAND", 20 => "STAND", 21 => "STAND"},
+  10 => {13 => "HIT", 14 => "HIT", 15 => "HIT", 16 => "HIT", 17 => "HIT", 18 => "HIT", 19 => "STAND", 20 => "STAND", 21 => "STAND"},
+  11 => {13 => "HIT", 14 => "HIT", 15 => "HIT", 16 => "HIT", 17 => "HIT", 18 => "STAND", 19 => "STAND", 20 => "STAND", 21 => "STAND"}}}
 
 puts "This is a game of Blackjack. Please enter the worth of the card - 2 to 10 or A, which is 11."
 
